c2c13bb7a958dcc26cbc9321b15730f66da87f4a
Igor Kuratov <kuratov.ia@dclouds.ru>
2023-02-21 13:38:02 +0700

    element plus styles and icons

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,8 @@ import 'virtual:fonts.css'
 import App from './App.vue'
 import router from './router'
 import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 
 import '@/styles/index.scss'
 
@@ -20,5 +22,9 @@ app.use(createPinia())
 app.use(ElementPlus)
 
 app.use(router)
+
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+  app.component(key, component)
+}
 
 app.mount('#app')
